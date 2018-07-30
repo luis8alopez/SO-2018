5df9030e68a99d73fefcf83893fa0871da945a93
Luis <leduardo.ochoa@udea.edu.co>
2018-07-30 12:57:33 -0500

    primer

---
+++ lab0.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// Función main
+
+int main(){
+    /*Codigo...*/
+    int carechimba=10;
+    printf("\n");
+    printf("Codebabes.com\n\n");
+    printf("Dato de prueba del puto c\n");
+    printf("Hay %d personas acá\n", carechimba);
+    return 0;
+
+}
